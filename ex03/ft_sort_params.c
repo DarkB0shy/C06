f1c0e12ecee03168080b416eb4b817cd2be1a128
Davide Carassiti <dcarassi@e3r4p4.42roma.it>
2022-10-27 20:07:59 +0200

    tentativi

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,13 +6,55 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 17:34:02 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/27 17:35:07 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/27 20:07:08 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	main (int argc, char *argv)
+int	ft_strcmp(char *s1, char *s2)
 {
-	
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	char	*temp;
+	int	j;
+
+	j = 1;
+	i = 1;
+	while (j < argc - 1)
+	{
+		while (i < argc - 1)
+		{
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			{
+				temp = &argv[i];
+				&argv[i] = &argv[i + 1];
+				&argv[i + 1] = temp;
+			}
+			i++;
+		}
+		j++;
+	}
+	i = 1;
+	while (argv[i])
+	{
+		argv[i]+='0';
+		write(1, &argv[i], 1);
+		i++;
+	}
+	return (0);
 }
