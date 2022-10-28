f2732f8130707b4235832f06d4afd01b49290195
Davide Carassiti <dcarassi@e3r4p4.42roma.it>
2022-10-28 12:14:23 +0200

    scizzerosei

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -5,24 +5,25 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/27 17:34:02 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/27 20:07:08 by dcarassi         ###   ########.fr       */
+/*   Created: 2022/10/28 11:17:24 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/28 12:13:32 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
+
 	int	i;
 
 	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] || s2[i])
 	{
-		if (s1[i] == s2[i])
-			i++;
-		if (s1[i] < s2[i] || s1[i] > s2[i])
-			return (s1[i] - s2[i]);
+		if (s1[i] > s2[i])
+			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -30,31 +31,25 @@ int	ft_strcmp(char *s1, char *s2)
 int	main(int argc, char **argv)
 {
 	int	i;
-	char	*temp;
 	int	j;
+	char *swap;
 
+	i = 0;
 	j = 1;
-	i = 1;
 	while (j < argc - 1)
 	{
-		while (i < argc - 1)
+		if ((ft_strcmp(argv[j], argv[j+1]) == 1))
 		{
-			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
-			{
-				temp = &argv[i];
-				&argv[i] = &argv[i + 1];
-				&argv[i + 1] = temp;
-			}
-			i++;
+			swap = argv[j];
+			argv[j] = argv[j + 1];
+			argv[j + 1] = swap;
 		}
 		j++;
 	}
 	i = 1;
 	while (argv[i])
-	{
-		argv[i]+='0';
-		write(1, &argv[i], 1);
+	{	
+		printf ("%s", argv[i]);
 		i++;
 	}
-	return (0);
 }
