0374593e5ceffdfa06fd7d98c90b7b3c6851c13c
Davide Carassiti <dcarassi@e3r8p5.42roma.it>
2022-10-31 12:11:04 +0100

    its lit

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -6,12 +6,22 @@
 /*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 11:17:24 by dcarassi          #+#    #+#             */
-/*   Updated: 2022/10/28 22:15:56 by dcarassi         ###   ########.fr       */
+/*   Updated: 2022/10/31 12:10:21 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+
+int	get_w_length(char *str)
+{
+	int	wcounter;
+
+	wcounter = 0;
+	while (str[wcounter])
+		wcounter++;
+	return (wcounter);
+}
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -20,7 +30,7 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] || s2[i])
 	{
-		if (s1[i] > s2[i])
+		if (s1[i] > s2[i] || s1[i] < s2[i])
 			return (1);
 		i++;
 	}
@@ -32,23 +42,31 @@ int	main(int argc, char **argv)
 	char	*swap;
 	int		i;
 	int		j;
+	int		k;
 
 	i = 0;
 	j = 1;
+	k = 1;
 	while (j < argc - 1)
 	{
-		if ((ft_strcmp(argv[j], argv[j + 1]) == 1))
-		{
-			swap = argv[j];
-			argv[j] = argv[j + 1];
-			argv[j + 1] = swap;
+		while (k < argc - 1 )
+		{	
+			if ((ft_strcmp(argv[j], argv[j + 1]) == 1))
+			{
+				swap = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = swap;
+			}
+			k++;
 		}
 		j++;
 	}
 	i = 1;
-	while (argv[i])
-	{	
-		printf ("%s", argv[i]);
+	while (i < argc)
+	{
+		write (1, argv[i], get_w_length(argv[i]));
+		write (1, "\n", 1);
 		i++;
 	}
+	return (0);
 }
